6e45f024472f29b54cb15d6aa68cd66804d0fd38
Damien Thouvenin <d.thouvenin@clt-services.com>
2013-10-16 15:34:41 +0200

    code plus propre, ajout quelques tests et bugfix si n>4

--- 2013-10-15 Coding breakfast #6/Vampires - solutions/Damien (C#)/Program.cs
+++ 2013-10-15 Coding breakfast #6/Vampires - solutions/Damien (C#)/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Vampires
 {
+    public struct Vampire
+    {
+        public Vampire(int a, int b, int v)
+        {
+            this.a = a;
+            this.b = b;
+            this.v = v;
+        }
+        public int a;
+        public int b;
+        public int v;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -15,65 +26,62 @@ namespace Vampires
             if(!int.TryParse(args[0], out nbDigits))
                 Error("Usage: Vampires n /* N: nombre pair de chiffres */");
             Info("Recherche des vampires à {0} chiffres", nbDigits);
-            var nbVampires = LookForVampires2(nbDigits/2);
-            Info("{0} nombres trouvés", nbVampires);
+            var vampires = LookForVampires(nbDigits/2)
+                .ToLookup(res => res.v, res => string.Format("{0}x{1}", res.a, res.b));
+            foreach(IGrouping<int, string> g in vampires)
+                Info("{0} = {1}", g.Key, string.Join(" ou ", g));           
+            Info("{0} nombres trouvés", vampires.Count);
             Console.ReadLine();
         }
-
-        static int LookForVampires1(int nbDigits)
+        
+        public static int Pow10(int n)
         {
-            var min = (Int32) Math.Pow(10, nbDigits-1);
-            var max = (Int32)Math.Pow(10, nbDigits);
-            var results = 0;
-            for (var a = min; a < max; a++)
-            {
-                int remainder;
-                var b = Math.DivRem(max*a, a - 1, out remainder);
-                if (remainder != 0)
-                    continue;
-                if(b >= max || (b%10) == 0)
-                    continue;
-                Info("{2} = {0}x{1}", a, b, a*b);
-                results++;
-            }
-            return results;
+            return n > 0 ? 10*Pow10(n - 1) : 1;
         }
 
-        static int LookForVampires2(int nbDigits)
+        public static IEnumerable<int> Digits(int n)
         {
-            var min = (Int32)Math.Pow(10, nbDigits - 1);
-            var max = (Int32)Math.Pow(10, nbDigits);
-            var results = 0;
+            do
+            {
+                yield return n%10;
+            } while ((n = n/10) > 0);
+        }
+
+        public static int[] SortedDigits(int n)
+        {
+            var res = Digits(n).ToArray();
+            Array.Sort(res);
+            return res;
+        }
+
+        public static int[] SortedDigits(int a, int b)
+        {
+            var res = Digits(a).Concat(Digits(b)).ToArray();
+            Array.Sort(res);
+            return res;
+        }
+
+        static IEnumerable<Vampire> LookForVampires(int nbDigits)
+        {
+            var min = Pow10(nbDigits - 1);
+            var max = 10*min;
             for (var a = min; a < max; a++)
             {
                 for(var b = a; b<max; b++)
                 {
                     if ((b % 10) == 0 && (a % 10) == 0)
                         continue;
-                    var fragmentDigits = new int[nbDigits * 2];
-                    var vampireDigits = new int[nbDigits * 2];
-                    var vampire = a*b;
-                    fragmentDigits[0] = a / min;
-                    fragmentDigits[1] = a % min;
-                    fragmentDigits[2] = b / min;
-                    fragmentDigits[3] = b % min;
-                    var vampireHigh = vampire / max;
-                    var vampireLow = vampire % max;
-                    vampireDigits[0] = vampireHigh / min;
-                    vampireDigits[1] = vampireHigh % min;
-                    vampireDigits[2] = vampireLow / min;
-                    vampireDigits[3] = vampireLow % min;
-                    Array.Sort(fragmentDigits);
-                    Array.Sort(vampireDigits);                    
+                    var vampire = a * b;
+                    var fragmentDigits = SortedDigits(a, b);
+                    var vampireDigits = SortedDigits(vampire);
                     if (!fragmentDigits.SequenceEqual(vampireDigits))
                         continue;
-                    Info("{2} = {0}x{1}", a, b, vampire);
-                    results++;
+                    yield return new Vampire(a, b, vampire);
                 }
             }
-            return results;
         }
 
+        #region utilitaires
         static void Info(string msgFmt, params object[] args)
         {
             Console.WriteLine(args.Length==0 ? msgFmt : string.Format(msgFmt, args));
@@ -85,6 +93,6 @@ namespace Vampires
             Info(msgFmt, args);
             Console.ResetColor();
         }
-
+        #endregion
     }
 }
---
+++ 2013-10-15 Coding breakfast #6/Vampires - solutions/Damien (C#)/VampireTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using MbUnit.Framework;
+
+namespace Vampires
+{
+    [TestFixture]
+    public class VampireTests
+    {
+        [Test]
+        public void DigitsAreExtractedCorrectly()
+        {
+            var number = 17650;
+            var expectedDigits = new[] {0, 5, 6, 7, 1};
+
+            var digits = Program.Digits(number);
+            
+            Assert.AreElementsEqual(expectedDigits, digits);
+        }
+
+        [Test]
+        public void ExtractedDigitsSortedCorrectly()
+        {
+            var number = 17250;
+            var expectedDigits = new[] { 0, 1, 2, 5, 7 };
+
+            var digits = Program.SortedDigits(number);
+
+            Assert.AreElementsEqual(expectedDigits, digits);
+        }
+
+        [Test]
+        public void CombinedDigitsSortedCorrectly()
+        {
+            var a = 172;
+            var b = 501;
+            var expectedDigits = new[] { 0, 1, 1, 2, 5, 7 };
+
+            var digits = Program.SortedDigits(a, b);
+
+            Assert.AreElementsEqual(expectedDigits, digits);
+        }
+    }
+}
