5da4f06740f12c4309cbd9d9015147781a6d9c2a
Damien Thouvenin <d.thouvenin@clt-services.com>
2014-02-19 15:43:22 +0100

    Première version en sortie du petit-dej (avec correction du Bug "OutOfMemory")

---
+++ 2014-02-18 Coding Breakfast #2014.2/Solutions/Damien et Cyrille - C#/ReversiBoard.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Reversi
+{
+    public enum Box { Empty, White, Black }
+    public struct Position
+    {
+        public Position(int col, int row)
+        {
+            Col = col;
+            Row = row;
+        }
+        public int Col;
+        public int Row;
+    }
+
+    public class ReversiBoard
+    {
+        private readonly Box[,] _positions = new Box[8, 8];
+
+        public ReversiBoard()
+        {
+            this[3, 3] = Box.Black;
+            this[4, 4] = Box.Black;
+            this[3, 4] = Box.White;
+            this[4, 3] = Box.White;
+        }
+        public Box this[int col, int row]
+        {
+            get { return _positions[col, row]; }
+            set { _positions[col, row] = value; }
+        }
+
+        public Box this[Position pos]
+        {
+            get { return _positions[pos.Col, pos.Row]; }
+            set { _positions[pos.Col, pos.Row] = value; }
+        }
+
+        /// <summary>
+        /// Retourne les positions libres autour du point considéré, triées dans l'ordre des aiguille d'une montre, en partant du coin en haut à gauche
+        /// </summary>
+        /// <param name="col">Colonne</param>
+        /// <param name="row">Ligne</param>
+        /// <returns></returns>
+        public IEnumerable<Position> PotentialPositionsFor(int col, int row)
+        {
+            return from pos in Neighbours(col, row) where this[pos] == Box.Empty select pos;
+        }
+
+        public IEnumerable<Position> ValidPositionsFor(int col, int row)
+        {
+            var color = this[col, row];
+            var results = new List<Position>();
+            foreach (var pos in PotentialPositionsFor(col, row).ToList())
+            {
+                var colOffset = col - pos.Col;
+                var rowOffset = row - pos.Row;
+                var candidate = new Position(col + colOffset, row + rowOffset);
+                while (candidate.Col >= 0 && candidate.Col < 8 && candidate.Row >= 0 && candidate.Row < 8)
+                {
+                    if (this[candidate] == Box.Empty)
+                        break;
+                    if (this[candidate] == color)
+                        candidate = new Position(candidate.Col + colOffset, candidate.Row + rowOffset);
+                    else
+                    {
+                        results.Add(pos);
+                        break;
+                    }
+                }
+            }
+            return results;
+        }
+
+
+        public IEnumerable<Position> Neighbours(int col, int row)
+        {
+            yield return new Position { Col = col - 1, Row = row - 1 };
+            yield return new Position { Col = col, Row = row - 1 };
+            yield return new Position { Col = col + 1, Row = row - 1 };
+            yield return new Position { Col = col + 1, Row = row };
+            yield return new Position { Col = col + 1, Row = row + 1 };
+            yield return new Position { Col = col, Row = row + 1 };
+            yield return new Position { Col = col - 1, Row = row + 1 };
+            yield return new Position { Col = col - 1, Row = row };
+        }
+    }
+}
---
+++ 2014-02-18 Coding Breakfast #2014.2/Solutions/Damien et Cyrille - C#/ReversiTestCases.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace Reversi
+{
+    [TestFixture]
+    public class ReversiTestCases
+    {
+        [Test]
+        public void New_Board_Is_All_Empty_Bu_The_Four_Central_Positions()
+        {
+            var board = new ReversiBoard();
+
+            for (var col = 0; col < 8;col++ )
+            {
+                for(var row=0;row<8;row++)
+                {
+                    if ((col == 3 || col == 4) && (row == 3 || row == 4))
+                        continue;
+                    Assert.AreEqual(Box.Empty, board[col, row]);
+                }
+            }                
+        }
+        
+        [Test]
+        public void When_New_Board_And_Considering_First_Black_There_Are_5_Eligible_Positions()
+        {
+            var board = new ReversiBoard();
+
+            var results = new List<Position>(board.PotentialPositionsFor(3, 3));
+
+            Assert.AreEqual(5, results.Count);
+            Assert.AreEqual(2, results[0].Col); Assert.AreEqual(2, results[0].Row);
+            Assert.AreEqual(3, results[1].Col); Assert.AreEqual(2, results[1].Row);
+            Assert.AreEqual(4, results[2].Col); Assert.AreEqual(2, results[2].Row);
+            Assert.AreEqual(2, results[3].Col); Assert.AreEqual(4, results[3].Row);
+            Assert.AreEqual(2, results[4].Col); Assert.AreEqual(3, results[4].Row);            
+        }
+
+        [Test]
+        public void When_New_Board_And_Considering_First_Black_There_Are_2_Valid_Positions()
+        {
+            var board = new ReversiBoard();
+
+            var results = new List<Position>(board.ValidPositionsFor(3, 3));
+            /*
+                . . . . . . . .
+                . . . . . . . .
+                . . . x . . . .
+                . . x N B . . .
+                . . . B N . . .
+                . . . . . . . .
+                . . . . . . . .
+             */ 
+            Assert.AreEqual(2, results.Count);
+            Assert.AreEqual(3, results[0].Col); Assert.AreEqual(2, results[0].Row);
+            Assert.AreEqual(2, results[1].Col); Assert.AreEqual(3, results[1].Row);
+        }
+    }
+}
