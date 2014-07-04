e1e9b3b84609548a4ac57f00e09c96495c5738ca
dedale <ded@laposte.net>
2014-07-04 22:25:40 +0200

    [MazeSolver] Right hand wall solution
    Done in 1 h in F#
    (I did not know yet how to implement interfaces in F#!)
    4 unit tests only
    4 lines of code in YourTurn()

---
+++ 2014-07-03 Coding Mojito #2/Solutions/dedale/MazeSolverFs/MazeSolver.fs
@@ -0,0 +1,21 @@
+﻿namespace MazeSolver
+
+open System
+open Mazes.Core
+
+type MazeSolver() =
+    let mutable _maze = null
+    let mutable _mouse = null
+    interface IMazeSolver with
+        member this.Init(maze, mouse) =
+            _maze <- maze
+            _mouse <- mouse
+        member this.YouLoose() =
+            ()
+        member this.YouWin() =
+            ()
+        member this.YourTurn() =
+            while not (_maze.CanIMove()) do
+                _mouse.TurnLeft()
+            _mouse.Move()
+            _mouse.TurnRight()
---
+++ 2014-07-03 Coding Mojito #2/Solutions/dedale/MazeSolverFs/MazeSolverTest.fs
@@ -0,0 +1,66 @@
+﻿module MazeSolverTest
+
+open MazeSolver
+open FsUnit
+open Xunit
+open Mazes.Core
+
+type MouseAction = Move | TurnRight | TurnLeft
+
+type MockMouse() =
+    let mutable actions = []
+    member this.Actions
+        with get() = actions
+    interface IMouse with
+        member this.Move() =
+            actions <- Move :: actions
+        member this.TurnLeft() =
+            actions <- TurnLeft :: actions
+        member this.TurnRight() =
+            actions <- TurnRight :: actions
+
+type MockMaze(moves : bool list) =
+    let mutable _moves = moves
+    interface IMaze with
+        member this.CanIMove() =
+            match _moves with
+            | b :: l ->
+                _moves <- l
+                b
+            | [] -> failwith "Out of moves"
+        member this.AmIOut() =
+            false
+
+[<Fact>]
+let ``init`` () =
+    let maze = MockMaze([])
+    let mouse = MockMouse()
+    let solver = MazeSolver() :> IMazeSolver
+    solver.Init(maze :> IMaze, mouse :> IMouse)
+
+[<Fact>]
+let ``move and turn right if can move`` () =
+    let maze = MockMaze([ true ])
+    let mouse = MockMouse()
+    let solver = MazeSolver() :> IMazeSolver
+    solver.Init(maze :> IMaze, mouse :> IMouse)
+    solver.YourTurn()
+    List.rev mouse.Actions |> should equal [ Move ; TurnRight ]
+
+[<Fact>]
+let ``turn left if cannot move`` () =
+    let maze = MockMaze([ false ; true ])
+    let mouse = MockMouse()
+    let solver = MazeSolver() :> IMazeSolver
+    solver.Init(maze :> IMaze, mouse :> IMouse)
+    solver.YourTurn()
+    List.rev mouse.Actions |> should equal [ TurnLeft ; Move ; TurnRight ]
+
+[<Fact>]
+let ``turn left while cannot move`` () =
+    let maze = MockMaze([ false ; false ; false ; true ])
+    let mouse = MockMouse()
+    let solver = MazeSolver() :> IMazeSolver
+    solver.Init(maze :> IMaze, mouse :> IMouse)
+    solver.YourTurn()
+    List.rev mouse.Actions |> should equal [ TurnLeft ; TurnLeft ; TurnLeft ; Move ; TurnRight ]
