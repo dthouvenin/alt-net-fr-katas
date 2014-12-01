17e256fac2435650792dd61027a91f6f51371c73
Damien Thouvenin <d.thouvenin@clt-services.com>
2014-12-01 18:30:44 +0100

    Ajout sujet et solution pourrie en exemple pour le coding chocolate

---
+++ 2014-12-02 Coding Chocolate/Solutions/Damien - Sapin tout pourri en 3 minutes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static private Random random = new Random(); 
+        static void Center(int width, string text)
+        {
+            var pad = (width + text.Length) / 2;
+            Console.WriteLine(text.PadLeft(pad));
+        }
+
+        static string BuildLine(int n)
+        {
+            var line = new char[n*2];
+            for (int i = 0; i < n; i++)
+            {
+                line[i*2] = random.Next(4096) < 512 ? '°' : '^';
+                line[i * 2 + 1] = ' ';
+            }
+            return new String(line);
+        }
+
+        static void Main(string[] args)
+        {            
+            Center(60, "*");
+            for (int i = 2; i < 20; i++)
+            {
+                Center(60, BuildLine(i));
+            }
+            Center(60, "XXX");
+            Center(60, "XXX");
+            Center(60, "XXX");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2014-12-02 Coding Chocolate/readme.md
@@ -0,0 +1,14 @@
+# Kata N°19.	Mon beau sapin, roi des forêts #
+Votre challenge est d’écrire un programme qui dessine un sapin. Vous pouvez le faire graphiquement ou en ASCII Art.   
+Le plus beau sapin gagne un bonnet rouge clignotant ;-)
+
+    
+# Kata N°20.	Les trois rois mages #
+Trois vieux bonhommes, nommés Gaspard, Melchior et Balthazar, se rendent aux grands magasins du boulevard Haussmann pour acheter des cadeaux (de l’or, de la myrrhe et de l’encens bien sûr). 
+
+Au moment de passer en caisse la vendeuse multiplie les prix des articles et leur annonce 65,52 €
+
+Mais Melchior, à qui on ne la fait pas, a remarqué le petit jeu de la vendeuse et lui dit « Et mademoiselle, vous avez multiplié les prix, au lieu de les additionner ».
+
+Celle-ci s’excuse et refait le calcul mais trouve toujours 65,52€. 
+Quels sont les prix des cadeaux ? 
